188fc54d6d74453513fceded347f0fe330cc48b4
raulboo <raulpschutz@hotmail.com>
2021-06-06 12:15:01 -0300

    finished car proto

--- prototypes/Car.gd
+++ prototypes/Car.gd
@@ -1,16 +1,23 @@
 extends KinematicBody2D
 
+export var acceleration : float = 5
+export var friction_coefficient : float = 0.95
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var velocity := Vector2(0, 0)
+onready var direction := Vector2(1, 0)
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(_delta):
+	velocity.y += 10 #Gravity
+	
+	if Input.is_action_pressed("action"):
+		#Acceleration
+		velocity += self.acceleration * self.direction
+	else:
+		velocity.x *= self.friction_coefficient
+		#Friction
+		
+	if Input.is_action_just_released("action"):
+		self.direction = -self.direction
+	
+	
+	velocity = move_and_slide(velocity)
